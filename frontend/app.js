ddb868e2adc7e38bb29546805ac1c6601cf42010
Bogi <bogdan.popovic@outlook.com>
2026-01-02 18:54:18 +0100

    Fixed JS

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,6 @@
 const api = "http://91.99.128.94:8000/";
 
-fetch(api).then(data => {
-    document.getElementById("message").innerHTML = data.message;
-})
+const response = await fetch(api);
+const data = await response.json()
+
+document.getElementById("message").innerHTML = data.message;
