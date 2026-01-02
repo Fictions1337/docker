7c01149275d0e3ce0560db95f73844ed3b0fa7c4
Bogi <bogdan.popovic@outlook.com>
2026-01-02 23:17:09 +0100

    Updated JS script

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,16 @@
 const api = "http://91.99.128.94:8000/";
 
-const response = await fetch(api);
-const data = await response.json()
-
-document.getElementById("message").innerHTML = data.message;
+fetch(api)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP Error " + response.status);
+        }
+        return response.json()
+    })
+    .then(data => {
+        document.getElementById("message").textContent = data.message;
+    })
+    .catch(error => {
+        console.error(error);
+        document.getElementById("message").textContent = "Failed to load message";
+    });
